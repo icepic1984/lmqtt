17d0ac16d0e9635d14d98ab95fda3560b0e2c999
icepic <icepic2009@googlemail.com>
2015-04-06 18:30:45 +0200

    Refactor Serialize/Deserialize

--- src/mqtt_serialize.hpp
+++ src/mqtt_serialize.hpp
@@ -2,7 +2,6 @@
 #define MQTT_SERIALIZE_HPP
 
 namespace lmqtt {
-
 namespace detail {
 
 template <typename InputIterator>
@@ -11,90 +10,153 @@ inline uint16_t read_big_endian(InputIterator start)
 	return(*(start+1) << 0 ) | *(start) << 8;
 }
 
-template <typename T,typename InputIterator>
+template <typename T>
+struct get_size_helper;
+
+template <typename T>
+struct get_size_helper
+{
+   static std::size_t get_size(const T value){
+	   return sizeof(value);
+   }
+};
+
+template <>
+struct get_size_helper<std::string>
+{
+   static std::size_t get_size(const std::string& value){
+	   return value.size();
+   }
+};
+
+template <>
+struct get_size_helper<std::vector<uint8_t>>
+{
+	static std::size_t get_size(const std::vector<uint8_t>& value) {
+		return value.size();
+	 }
+
+};
+
+template <typename InputIterator, typename T>
 struct deserialize_helper;
 
-template <typename T, typename InputIterator>
+template <typename InputIterator, typename T>
 struct deserialize_helper
 {
-   static T deserialize(InputIterator begin){
-	   return(*begin);
-   }
+	static InputIterator deserialize(InputIterator begin,
+	                                 InputIterator end, T& value) {
+		std::size_t size = get_size_helper<T>::get_size(value);
+		if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
+		value = *begin;
+		return begin+size;
+	}
 };
 
 template <typename InputIterator>
-struct deserialize_helper<uint16_t,InputIterator>
+struct deserialize_helper<InputIterator,uint16_t>
 {
-   static uint16_t deserialize(InputIterator begin){
-	   return(read_big_endian(begin));
-   }
+	static InputIterator deserialize(InputIterator begin,
+	                                 InputIterator end, uint16_t& value) {
+		std::size_t size = get_size_helper<uint16_t>::get_size(value);
+		if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
+		value = read_big_endian(begin);
+		return begin+size;
+	}
 };
 
 template <typename InputIterator>
-struct deserialize_helper<std::string, InputIterator>
+struct deserialize_helper<InputIterator,std::string>
 {
-   static std::string deserialize(InputIterator begin, std::size_t n){
-	   std::string str(n,' ');
-	   for(std::size_t i = 0; i < n; ++i){
-		   str[i] = *(begin+i);
+   static InputIterator deserialize(InputIterator begin, InputIterator end,
+                                    std::string& value){
+	   std::size_t size =  get_size_helper<std::string>::get_size(value);
+	   if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
+	   for(std::size_t i = 0; i < size; i++){
+		   value[i] = *(begin+i);
 	   }
-	   return str;
+	   return begin+size;
    }
 };
 
-template <typename T, typename InputIterator>
+
+template <typename InputIterator>
+struct deserialize_helper<InputIterator,std::vector<uint8_t>>
+{
+   static InputIterator deserialize(InputIterator begin, InputIterator end,
+                                    std::vector<uint8_t>& value){
+	   std::size_t size = get_size_helper<std::vector<uint8_t>>::get_size(value);
+	   if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
+	   for(std::size_t i = 0; i < size; i++){
+		   value[i] = *(begin+i);
+	   }
+	   return begin+size;
+   }
+};
+
+template <typename InputIterator, typename T>
 struct serialize_helper;
 
 template <typename InputIterator>
-struct serialize_helper<uint8_t,InputIterator>
+struct serialize_helper<InputIterator, uint8_t>
 {
-   static void serialize(InputIterator begin, uint8_t value){
-	   	const uint8_t* value_begin = reinterpret_cast<const uint8_t*>(&value);
-		std::copy(value_begin,value_begin + 1, begin);
+   static InputIterator serialize(InputIterator begin, InputIterator end, uint8_t value){
+	   std::size_t size = get_size_helper<uint8_t>::get_size(value);
+	   if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
+	   const uint8_t* value_begin = reinterpret_cast<const uint8_t*>(&value);
+	   std::copy(value_begin,value_begin + size, begin);
+	   return begin+size;
    }
 };
 
 template <typename InputIterator>
-struct serialize_helper<uint16_t,InputIterator>
+struct serialize_helper<InputIterator,uint16_t>
 {
-   static void serialize(InputIterator begin, uint16_t value){
+   static InputIterator serialize(InputIterator begin, InputIterator end,
+                                  uint16_t value){
+	   std::size_t size = get_size_helper<uint16_t>::get_size(value);
+	   if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
 	   uint16_t tmp = ntohs(value);
 	   const uint8_t* value_begin = reinterpret_cast<const uint8_t*>(&tmp);
 	   std::copy(value_begin,value_begin + sizeof(uint16_t), begin);
+	   return begin+size;
    }
 };
 
 template <typename InputIterator>
-struct serialize_helper<std::string,InputIterator>
+struct serialize_helper<InputIterator,std::string>
 {
-   static void serialize(InputIterator begin, const std::string& value){
-	   std::size_t size = value.size();
+   static InputIterator serialize(InputIterator begin, InputIterator end,
+                                  const std::string& value){
+	   std::size_t size = get_size_helper<std::string>::get_size(value);
+	   if(std::distance(begin,end) < size) throw std::out_of_range("Out of range");
 	   const uint8_t* value_begin = reinterpret_cast<const uint8_t*>(value.data());
 	   std::copy(value_begin,value_begin + size, begin);
+	   return begin+size;
    }
 };
 
 }
 
 template <typename InputIterator,typename T>
-inline void serialize(InputIterator begin, const T& value)
+inline InputIterator serialize(InputIterator begin, InputIterator end,
+                               const T& value)
 {
-	detail::serialize_helper<T,InputIterator>::serialize(begin,value);
+	return(detail::serialize_helper<InputIterator,T>::serialize(begin,end,value));
 }
 
-template <typename T,typename InputIterator>
-inline T deserialize(InputIterator begin)
+template <typename InputIterator, typename T>
+inline InputIterator deserialize(InputIterator begin, InputIterator end, T& value)
 {
-	return detail::deserialize_helper<T,InputIterator>::deserialize(begin);
+	static_assert(std::is_same<T,std::string>() ||
+	              std::is_same<T,std::vector<uint8_t>>() ||
+	              std::is_same<T,uint8_t>() ||
+	              std::is_same<T,uint16_t>(), "Type not supported");
+	return detail::deserialize_helper<InputIterator,T>::
+	   deserialize(begin,end,value);
 }
 
-template <typename T,typename InputIterator>
-inline T deserialize(InputIterator begin,std::size_t n)
-{
-	return detail::deserialize_helper<T,InputIterator>::deserialize(begin,n);
-}
-	      
-}
 
+}
 
 #endif
--- test/serialize.cpp
+++ test/serialize.cpp
@@ -7,27 +7,47 @@
 
 BOOST_AUTO_TEST_CASE(serialize_test)
 {
-	std::vector<uint8_t> buffer(20,0);
-	// Test byte serialization
+	 std::vector<uint8_t> buffer(20,0);
+	/// Test byte serialization
 	uint8_t byte1 = 0;
 	uint8_t byte2 = 255;
-	lmqtt::serialize(buffer.begin(),byte1);
-	lmqtt::serialize(buffer.begin()+1,byte2);	
-	BOOST_CHECK_EQUAL(lmqtt::deserialize<uint8_t>(buffer.begin()),byte1);	
-	BOOST_CHECK_EQUAL(lmqtt::deserialize<uint8_t>(buffer.begin()+1),byte2);	
+	auto begin = lmqtt::serialize(buffer.begin(),buffer.end(),byte1);
+	begin = lmqtt::serialize(begin,buffer.end(),byte2);	
 
-	// Test uint16_t aka big endian
+	uint8_t check1;
+	uint8_t check2;
+	begin = lmqtt::deserialize(buffer.begin(),buffer.end(),check1);
+	begin = lmqtt::deserialize(begin,buffer.end(),check2);
+	BOOST_CHECK_EQUAL(check1,byte1);	
+	BOOST_CHECK_EQUAL(check2,byte2);	
+
+	// // Test uint16_t aka big endian
 	uint16_t twobyte1 = 0;
 	uint16_t twobyte2 = 65536-1;
-	lmqtt::serialize(buffer.begin(),twobyte1);
-	lmqtt::serialize(buffer.begin()+ sizeof(twobyte1),twobyte2);	
-	BOOST_CHECK_EQUAL(lmqtt::deserialize<uint16_t>(buffer.begin()),twobyte1);	
-	BOOST_CHECK_EQUAL(lmqtt::deserialize<uint16_t>(buffer.begin() +
-	                                               sizeof(twobyte1)),twobyte2);	
+	uint16_t check_two1 = 0;
+	uint16_t check_two2 = 0; 
+	begin= lmqtt::serialize(buffer.begin(),buffer.end(),twobyte1);
+	begin = lmqtt::serialize(begin,buffer.end(),twobyte2);	
+	begin = lmqtt::deserialize(buffer.begin(),buffer.end(),check_two1);
+	begin = lmqtt::deserialize(begin,buffer.end(),check_two2);
+	BOOST_CHECK_EQUAL(check_two1,twobyte1);	
+	BOOST_CHECK_EQUAL(check_two2,twobyte2);	
+
 	// Test string
 	std::string str("blalalalallalal");
-	lmqtt::serialize(buffer.begin(),str);	
-	BOOST_CHECK_EQUAL(lmqtt::deserialize<std::string>(
-		                  buffer.begin(), str.size()), str);
+	std::string test_str(str.size(),' ');
+	begin = lmqtt::serialize(buffer.begin(),buffer.end(),str);	
+	lmqtt::deserialize(buffer.begin(), buffer.end(),test_str); 
+	BOOST_CHECK_EQUAL(test_str,str);
+
+	// Overflow
+	str = std::string("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	test_str = std::string(str.size(),' ');
+	//lmqtt::serialize(buffer.begin(), str);	
+	BOOST_CHECK_THROW(lmqtt::deserialize(buffer.begin(), buffer.end(),test_str),
+	                  std::out_of_range);
+	BOOST_CHECK_THROW(lmqtt::serialize(buffer.begin(),buffer.end(),test_str),
+	                  std::out_of_range);
+
 }
 
